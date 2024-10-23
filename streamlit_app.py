5e96c1e48940b7205ac64a1d647933d232c3a39e
Roman <49252234+roman7011@users.noreply.github.com>
2024-10-23 08:42:05 +0000

    Prva verzia aplikacie

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,49 @@
 import streamlit as st
 
-st.title("🎈 My new app 2")
+import pandas as pd
+
+import plotly.express as px
+
+import matplotlib.pyplot as plt
+
+st.title(" Ahoj Svet a Streamlit")
+
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+
+    "Toto je moja prvá aplikácia v Streamlite"
+
 )
+
+data = pd.DataFrame({
+
+    "Kategorie" : ["A", "B", "C", "D"],
+    "Hodnoty" : [25, 40, 35, 30]
+
+})
+
+st.write("Tabuľka s dátami:")
+
+st.dataframe(data)
+
+fig = px.bar(
+
+    data, x ="Kategorie", 
+    y = "Hodnoty", 
+    title = "Stlpcovy graf plotly"
+
+)
+
+st.plotly_chart(fig)
+
+if st.button("Infomacne okno"):
+    st.info("Toto je informacne okno")
+
+if st.button("Upozornovacie okno"):
+    st.warning("Toto je upozornovacie okno")
+
+if st.button("Chybove okno"):
+    st.error("Toto je chybove okno")
+
+
+
+
